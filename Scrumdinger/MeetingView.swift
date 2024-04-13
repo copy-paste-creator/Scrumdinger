e1a30874e4dd4499adde9aaca9e99f55a328dc82
nani <siva.apps@icloud.com>
2024-04-13 22:38:41 +0530

    stacks

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -12,16 +12,33 @@ struct MeetingView: View {
         VStack {
             ProgressView(value:10, total: 15)
             HStack{
-                VStack {
+                VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
+                        .font(.caption)
                     Label("300", systemImage: "hourglass.tophalf.fill")
                 }
-                VStack {
+                Spacer()
+                VStack(alignment: .trailing) {
                     Text("Seconds Remaining")
+                        .font(.caption)
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
+            .accessibilityElement(children: /*@START_MENU_TOKEN@*/.ignore/*@END_MENU_TOKEN@*/)
+            .accessibilityLabel("Time Remaining")
+            .accessibilityValue("10 minutes")
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack{
+                Text("Speaker 1 0f 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next Speaker")
+            }
         }
+        .padding()
     }
 }
 
